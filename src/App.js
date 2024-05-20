9f22279bf097ab8da27062ca4ffeefad9519b521
makachikava <chikavamakanaka@gmail.com>
2024-05-20 14:01:35 -0700

    added profile content

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div id='body'>
       <nav className='flex justify-between h-[80px] bg-black drop-shadow-xl'>
-        <div className='flex items-center ml-4'>
-          <span className='font-semibold text-white'>Mazvita Strohmeyer</span>
+        <div id='logo-container' className='flex items-center ml-4'>
+          <span id='logo' className='font-semibold text-white'>Mazvita Strohmeyer</span>
         </div>
 
-        <div className='flex items-center justify-between mr-4 w-[300px]'>
-          <h1 className='text-white'>Profile</h1>
-          <h1 className='text-white'>About</h1>
-          <h1 className='text-white'>Experience</h1>
-          <h1 className='text-white'>Contact</h1>
+        <div id='nav-links-container' className='flex items-center justify-between mr-4 w-[300px]'>
+          <h1 id='nav-links' className='text-white'>Profile</h1>
+          <h1 id='nav-links' className='text-white'>About</h1>
+          <h1 id='nav-links' className='text-white'>Experience</h1>
+          <h1 id='nav-links' className='text-white'>Contact</h1>
         </div>
       </nav>
 
       <section id='profile'>
+        <div>
+          <div id='profile-pic-container'>
+            image
+          </div>
 
+          <div id='headline-container'>
+            <p>Hello I'm</p>
+            <h1 id='name'>Mazvita Strohmeyer</h1>
+            <h1 id='title'>Talent Acquisitionist</h1>
+
+            <div id='headline-btn-container'>
+              <button id='resume-btn'>Download CV</button>
+              <button id='contact-btn'>Contact Info</button>
+            </div>
+          </div>
+
+        </div>
       </section>
       <section id='about'>
 
